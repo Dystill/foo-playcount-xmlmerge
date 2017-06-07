9b99672bc42785e79d5b488a6831933819346ca7
James Robinson <jameslrob95@gmail.com>
2017-06-07 17:48:26 -0400

    Reads data from file
    + Saves version and mapping as variables
    + Stores all entry data into a qmap with ID as key

--- playStatMerger/filereader.h
+++ playStatMerger/filereader.h
@@ -11,6 +11,7 @@
 // data structure to hold playback statistics for each song entry in the export xml file
 struct EntryStatistics {
     int count = 0;
+    int rating;
     int firstPlayed;
     int lastPlayed;
     int added;
@@ -19,16 +20,38 @@ struct EntryStatistics {
 class FileReader : public QXmlStreamReader
 {
 private:
-    QMap<QString,EntryStatistics> entries;  // holds a map pairing an entry's ID to it's playback data
-    QMap<QString,EntryStatistics> readFile(QString filePath);
+    QMap<QString,EntryStatistics *> entries;  // holds a map pairing an entry's ID to it's playback data
+    QMap<QString,EntryStatistics *> readFile(QString filePath);
 
-    int fileSize;
+    // XML tag strings
+    const QString PARENT_TAG = "PlaybackStatistics";
+    const QString VERSION = "Version";
+    const QString MAPPING = "Mapping";
+
+    const QString ENTRY_TAG = "Entry";
+    const QString ID = "ID";
+    const QString COUNT = "Count";
+    const QString RATING = "Rating";
+    const QString ADDED = "Added";
+    const QString FIRST_PLAYED = "FirstPlayed";
+    const QString LAST_PLAYED = "LastPlayed";
+
+    // meta data
+    QString versionNumber;
+    QString mappingString;
+    int fileSize = 0;
+    int entryCount = 0;
+    int totalPlays = 0;
 
 public:
     FileReader();
     FileReader(QString filePath);
-    QMap<QString, EntryStatistics> getEntries() const;
+    QMap<QString, EntryStatistics *> getEntries() const;
     int getFileSize() const;
+    int getEntryCount() const;
+    QString getVersion() const;
+    QString getMapping() const;
+    int getTotalPlays() const;
 };
 
 #endif // FILEREADER_H
